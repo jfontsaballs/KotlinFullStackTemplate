5dac67cc2df0a10b2d56623e932c251c3b933adb
jfontsaballs <jfontsaballs@gmail.com>
2022-03-13 21:10:14 +0100

    Apagada del Ktor

--- servidor/src/main/kotlin/Main.kt
+++ servidor/src/main/kotlin/Main.kt
@@ -5,13 +5,31 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
+    var keepRunning = true
+    var counter = 0
+
+    val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello, world!")
+                call.respondText("Hello, world!\n$counter")
+            }
+            get("/stop") {
+                keepRunning = false
+                call.respondText("Stopping")
             }
         }
-    }.start(wait = true)
+    }.start()
+
+    runBlocking {
+        while (keepRunning) {
+            println("Working ${counter++}")
+            delay(500)
+        }
+    }
+
+    server.stop(1000, 3000)
 }
