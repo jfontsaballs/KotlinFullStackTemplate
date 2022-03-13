6893b4d5dbb1f8dfb43caaba686094ea90ab33cd
jfontsaballs <jfontsaballs@gmail.com>
2022-03-13 21:31:39 +0100

    Afegir compilació bàsica de JavaScript

--- servidor/src/main/kotlin/Main.kt
+++ servidor/src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
 package com.test
 
 import io.ktor.application.*
+import io.ktor.html.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.html.*
 
 fun main() {
     var keepRunning = true
@@ -15,11 +18,31 @@ fun main() {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello, world!\n$counter")
+                call.respondHtml {
+                    head {
+                        meta { charset = "utf-8" }
+                        title { +"Kotlin full stack application demo" }
+                    }
+                    body {
+                        div {
+                            style = "display: flex; justify-content: center;"
+                            div {
+                                style = "margin: 10%; font-size: 48px;"
+                                id = "root"
+                                +"Loading..."
+                            }
+                        }
+                        script(src = "/client.js") { }
+                    }
+                }
             }
             get("/stop") {
                 keepRunning = false
                 call.respondText("Stopping")
+            }
+
+            static("/") {
+                resources("/")
             }
         }
     }.start()
